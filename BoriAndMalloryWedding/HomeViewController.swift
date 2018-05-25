e1ac33793fc16125a865dd20cfe4c3373369b31d
borikanes <ooludemi@terpmail.umd.edu>
2018-05-25 01:18:21 -0400

    added images in sketch file

---
+++ BoriAndMalloryWedding/HomeViewController.swift
@@ -0,0 +1,37 @@
+//
+//  HomeViewController.swift
+//  BoriAndMalloryWedding
+//
+//  Created by Bori Oludemi on 5/24/18.
+//  Copyright © 2018 borikanes. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet var topImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
