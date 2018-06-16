ebd6a9402f7d7a63f19d4dfa4be76484f7d2997a
borikanes <ooludemi@terpmail.umd.edu>
2018-06-15 22:02:27 -0400

    added some modifications for ipad 12.9

--- BoriAndMalloryWedding/ViewControllers/ScheduleViewController.swift
+++ BoriAndMalloryWedding/ViewControllers/ScheduleViewController.swift
@@ -19,8 +19,11 @@ class ScheduleViewController: UIViewController {
     @IBOutlet var horsDoeuvresBaseView: UIView!
     @IBOutlet var nigerianCeremonyBaseView: GradientView!
     @IBOutlet var receptionBaseView: GradientView!
+    @IBOutlet var umdChapelLabel: UILabel!
 
     let iphoneSmallFont = CGFloat(30)
+    let ipad129FontSmall = CGFloat(40)
+    let ipad129Font = CGFloat(80)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +39,12 @@ class ScheduleViewController: UIViewController {
         if isIphone5AndBelow() {
             churchLabel.font = churchLabel.font.withSize(iphoneSmallFont)
             nigerianTraditionalLabel.font = nigerianTraditionalLabel.font.withSize(iphoneSmallFont)
+        } else if isIpadPro129() {
+            umdChapelLabel.font = umdChapelLabel.font.withSize(ipad129FontSmall)
+            lunchLabel.font = lunchLabel.font.withSize(ipad129Font)
+            churchLabel.font = churchLabel.font.withSize(ipad129Font)
+            horsDoeuvresLabel.font = horsDoeuvresLabel.font.withSize(ipad129Font)
+            nigerianTraditionalLabel.font = nigerianTraditionalLabel.font.withSize(75)
         }
 
     }
