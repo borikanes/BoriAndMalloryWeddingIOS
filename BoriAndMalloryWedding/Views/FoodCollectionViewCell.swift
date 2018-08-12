3f9615bf387adf139c1a0e66a3e1019e3f4cacfd
borikanes <ooludemi@terpmail.umd.edu>
2018-08-12 17:07:31 -0400

    added gradients to the food VC, thanks mike

--- BoriAndMalloryWedding/Views/FoodCollectionViewCell.swift
+++ BoriAndMalloryWedding/Views/FoodCollectionViewCell.swift
@@ -13,4 +13,13 @@ class FoodCollectionViewCell: UICollectionViewCell {
     @IBOutlet var foodName: UILabel!
     @IBOutlet var angleIndicatorImageView: UIImageView!
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+//        let gradientLayer = CAGradientLayer()
+//        gradientLayer.frame = CGRect(x: 0, y: 0, width: foodImage.frame.width, height: foodImage.frame.height)
+//        gradientLayer.colors = [UIColor.clear.withAlphaComponent(0.0).cgColor, UIColor.black.cgColor]
+//        gradientLayer.locations = [0.0, 0.9]
+//        foodImage.layer.addSublayer(gradientLayer)
+    }
 }
